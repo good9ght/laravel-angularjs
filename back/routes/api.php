869d7da90d0a3f94415e029be62a7d32fd757190
Victor <victor.ananias8@gmail.com>
2018-02-16 16:01:27 -0200

    alterei o front

--- back/routes/api.php
+++ back/routes/api.php
@@ -13,8 +13,8 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-Route::get('/login', 'Auth/LoginController@login');
+// Route::middleware('auth:api')->get('/user', function () {
+//     return \Auth::user();
+// });
+//
+// Route::post('/login', 'Auth\LoginController@login');
